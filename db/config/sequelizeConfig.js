1667a6fcdfe614932176fcdefbfc2295459790c3
axeqwe1 <axeqwe@hotmail.com>
2024-10-12 16:30:20 +0700

    setconfigDocker

--- db/config/sequelizeConfig.js
+++ db/config/sequelizeConfig.js
@@ -2,9 +2,9 @@ const { Sequelize } = require('sequelize');
 //อันนี้เป็นส่วนที่ใช้ในการบอก Sequelize ว่าเราจะ connect ไปที่ Database ไหน
 const sequelize = new Sequelize("repaircomputer_db", "root", "root", 
   {
-    host: "localhost",
+    host: "db",
     dialect: "mysql",
-    port:8006,
+    port:3306,
     timezone: '+07:00'
   });
 
