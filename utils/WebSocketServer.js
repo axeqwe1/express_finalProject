1d6d5ad13b33e16d830e28cc280e45dfb93a41f6
axeqwe1 <axeqwe@hotmail.com>
2024-10-21 12:53:48 +0700

    configserver

--- utils/WebSocketServer.js
+++ utils/WebSocketServer.js
@@ -5,4 +5,16 @@ const wss = new WebSocket.Server({
     host: '0.0.0.0'  // ฟังทุกอินเตอร์เฟส
 });
 
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('message', (message) => {
+        console.log('Received:', message);
+        ws.send('Hello from server!');
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
 module.exports = wss;
